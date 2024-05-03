e1fae48e49edaa5b20b8c5632fd7b4729cea5513
ainhoavegasanchez <ainhoavega99@gmail.com>
2024-05-03 23:30:07 +0200

    1

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // Manejar todas las rutas (cualquier ruta que no coincida con un archivo estático) para servir el archivo 'index.html' del proyecto Angular
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/app/index.html'));
+  res.sendFile(path.join(__dirname, 'dist\app\browser\index.html'));
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
