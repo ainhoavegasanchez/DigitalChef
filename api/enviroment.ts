1498cd323739c4ce78db02bbc6d2d99dc21a91bd
ainhoavegasanchez <ainhoavega99@gmail.com>
2024-06-04 20:56:59 +0200

    carpeta cendor actualizada

--- api/enviroment.ts
+++ api/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false, 
-    API_URL: 'https://vps-65482c69.vps.ovh.net/manduca/Recursos' ,
+    API_URL: 'https://vps-65482c69.vps.ovh.net/php/Recursos__php' ,
     mapBoxToken:'pk.eyJ1IjoiYWluaG9hdmVnYTk5IiwiYSI6ImNsd3o4OXoxNDA0c2oycXM2bnBoaWR3dHcifQ.v6F8_kVSwJfYFJSTWPySTA'
   };
