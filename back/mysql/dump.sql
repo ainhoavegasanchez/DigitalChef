ff77b1375437807dfa8cc357f473fa765c57bc27
ainhoavegasanchez <ainhoavega99@gmail.com>
2024-06-07 16:51:39 +0200

    sin dump

--- back/mysql/dump.sql
+++ back/mysql/dump.sql
@@ -52,16 +52,16 @@ CREATE TABLE VALORACION(
     FOREIGN KEY  (id_producto) REFERENCES PRODUCTO(id)
 );
 
-INSERT INTO USUARIO(id,nombre, email, pass)VALUES(1,"pepe", "pepe@gmail.com", "123");
+-- INSERT INTO USUARIO(id,nombre, email, pass)VALUES(1,"pepe", "pepe@gmail.com", "123");
 
-INSERT INTO CATEGORIA(nombre)VALUES("categoria");
+-- INSERT INTO CATEGORIA(nombre)VALUES("categoria");
 
-INSERT INTO PRODUCTO(nombre, foto, descripcion, precio, id_catego)VALUES("prueba", "prueba.jpg", "descripcion producto prueba", 12.4, 1);
-INSERT INTO PRODUCTO(nombre, foto, descripcion, precio, id_catego)VALUES("Yakitori", "foto.jpg", "descripcion del Producto 2", 20.49, 1);
-INSERT INTO PRODUCTO(nombre, foto, descripcion, precio, id_catego)VALUES("producto 3", "foto3.jpg", "descripcion del producto 3", 3.33, 1);
+-- INSERT INTO PRODUCTO(nombre, foto, descripcion, precio, id_catego)VALUES("prueba", "prueba.jpg", "descripcion producto prueba", 12.4, 1);
+-- INSERT INTO PRODUCTO(nombre, foto, descripcion, precio, id_catego)VALUES("Yakitori", "foto.jpg", "descripcion del Producto 2", 20.49, 1);
+-- INSERT INTO PRODUCTO(nombre, foto, descripcion, precio, id_catego)VALUES("producto 3", "foto3.jpg", "descripcion del producto 3", 3.33, 1);
 
 
 
-INSERT INTO VALORACION(id_usuario, id_producto, valor)VALUES(1,2,3);
+-- INSERT INTO VALORACION(id_usuario, id_producto, valor)VALUES(1,2,3);
 
-INSERT INTO VALORACION(id_producto, id_usuario, valor)VALUES(3, 1, 5);
+-- INSERT INTO VALORACION(id_producto, id_usuario, valor)VALUES(3, 1, 5);
